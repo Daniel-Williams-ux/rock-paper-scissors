6f4ee3f62ddc5731b3fdedc14f913fcc130cb5a3
EC2 Default User <ec2-user@ip-172-31-32-227.ec2.internal>
2023-07-24 05:50:35 +0000

    updated play again button

--- script.js
+++ script.js
@@ -38,19 +38,25 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+// ... (previous code remains unchanged)
+
 function displayResult(result, playerSelection, computerSelection) {
   playerSign.textContent = playerSelection === 'Rock' ? "✊" : playerSelection === 'Paper' ? "✋" : "✌";
   computerSign.textContent = computerSelection === 'Rock' ? "✊" : computerSelection === 'Paper' ? "✋" : "✌";
   playerScoreElement.textContent = `Player: ${playerScore}`;
   computerScoreElement.textContent = `Computer: ${computerScore}`;
   scoreInfo.textContent = result;
+
   if (playerScore === 5 || computerScore === 5) {
-    const winner = playerScore === 5 ? "You" : "Computer";
-    endgameMsg.textContent = `${winner} win the game! Final score: You ${playerScore}, Computer ${computerScore}`;
-    endgameModal.classList.add('show');
-    overlay.classList.add('show');
-  }
+  const winner = playerScore === 5 ? "You" : "Computer";
+  endgameMsg.textContent = `${winner} win the game! Final score: You ${playerScore}, Computer ${computerScore}`;
+  endgameModal.classList.add('active'); // Add the 'show' class to display the modal
+  overlay.classList.add('active'); // Add the 'show' class to display the overlay
+ }
+ 
+
 }
+
 
 function resetGame() {
   playerScore = 0;
@@ -59,26 +65,40 @@ function resetGame() {
   computerScoreElement.textContent = 'Computer: 0';
   scoreInfo.textContent = 'Select your weapon';
   scoreMessage.textContent = 'First to score 5 points wins the game';
-  endgameModal.classList.remove('show');
-  overlay.classList.remove('show');
+  endgameModal.classList.remove('active')
+  overlay.classList.remove('active')
 }
 
+
 rockBtn.addEventListener('click', () => {
+  if (playerScore >= 5 || computerScore >= 5) {
+    return; // Game is over, don't proceed with new rounds
+  }
+
   const computerSelection = computerPlay();
   const result = playRound('Rock', computerSelection);
-  displayResult(result, 'Rock');
+  displayResult(result, 'Rock', computerSelection);
 });
 
 paperBtn.addEventListener('click', () => {
+  if (playerScore >= 5 || computerScore >= 5) {
+    return; // Game is over, don't proceed with new rounds
+  }
+
   const computerSelection = computerPlay();
   const result = playRound('Paper', computerSelection);
-  displayResult(result, 'Paper');
+  displayResult(result, 'Paper', computerSelection);
 });
 
 scissorsBtn.addEventListener('click', () => {
+  if (playerScore >= 5 || computerScore >= 5) {
+    return; // Game is over, don't proceed with new rounds
+  }
+
   const computerSelection = computerPlay();
   const result = playRound('Scissors', computerSelection);
-  displayResult(result, 'Scissors');
+  displayResult(result, 'Scissors', computerSelection);
 });
+
 
 restartBtn.addEventListener('click', resetGame);
